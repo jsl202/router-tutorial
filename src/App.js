8f562deb9006ef55901c62e8fa8dc8222bb78e3a
jsl202 <jsl202@naver.com>
2023-12-11 11:21:41 +0900

    router url parameter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profiles/:username" element={<Profile />} />
     </Routes>
-  )
-}
+  );
+};
 export default App;
