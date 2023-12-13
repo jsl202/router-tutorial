37c8fc2dd0bbbf398bf7e887d5b3c70c3fac6e88
jsl202 <jsl202@naver.com>
2023-12-13 10:17:13 +0900

    final?

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
 import Layout from "./Layout";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
   return (
@@ -17,6 +20,9 @@ const App = () => {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="mypage" element={<MyPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
